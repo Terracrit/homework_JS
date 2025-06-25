19368639843c34867d40b03f5a01c5abda7c67ee
Terracrit <149900990+Terracrit@users.noreply.github.com>
2025-06-25 22:43:43 +0200

    Initial commit

---
+++ homework_2.js
@@ -0,0 +1,81 @@
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах 1, 0, -3.
+
+//let value = 0;
+//if (value > 0) {
+//  console.log(true);
+//} else {
+//  console.log(false);}
+
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах 'test',"qwerty",true
+//let value = true;
+//if (value == "test") {
+// console.log(true);
+//} else {
+//  console.log(false);
+//}
+
+// Якщо змінна більше 10 - відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах 1, 10, 13.
+//let value = 10;
+//if (value > 10) {
+//value = value - 5;
+//console.log(value);
+//} else {
+//value = value + 5;
+//console.log(value);
+//}
+
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+
+// let value = prompt("Введіть число від 1 до 12");
+// switch (Number(value)) {
+//   case 1:
+//     console.log("Січень");
+//     break;
+//   case 2:
+//     console.log("Лютий");
+//     break;
+//   case 3:
+//     console.log("Березень");
+//     break;
+//   case 4:
+//     console.log("Квітень");
+//     break;
+//   case 5:
+//     console.log("Травень");
+//     break;
+//   case 6:
+//     console.log("Червень");
+//     break;
+//   case 7:
+//     console.log("Липень");
+//     break;
+//   case 8:
+//     console.log("Серпень");
+//     break;
+//   case 9:
+//     console.log("Вересень");
+//     break;
+//   case 10:
+//     console.log("Жовтень");
+//     break;
+//   case 11:
+//     console.log("Листопад");
+//     break;
+//   case 12:
+//     console.log("Грудень");
+//     break;
+// }
+
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+
+// let value = Number(prompt("Введіть тьохзначне число"));
+// let c = value % 10;
+// let v = Math.floor((value % 100) / 10);
+// let x = Math.floor(value / 100);
+// console.log(c + v + x);
